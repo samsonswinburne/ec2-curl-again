5a17948b7e80d0738db4bd74e0f4b4c6cc59f1e1
samsonswinburne <161039641+samsonswinburne@users.noreply.github.com>
2024-10-05 19:01:39 +1000

    Add files via upload

--- mydb.php
+++ mydb.php
@@ -6,34 +6,29 @@
 */
 require 'photo.php';
 require_once 'constants.php';
+require_once 'utils.php';
 
 class MyDB 
 {
-	private $dbh; 
+	private $dbh;
+	private $utils;
 	
-	// Constructor, establish a connection to the database in RDS
 	public function __construct() {
 		try {
+			$this->utils = new Utils();
 			$dsn = "mysql:host=".DB_ENDPOINT.";dbname=".DB_NAME;
 			$this->dbh = new PDO ( $dsn, DB_USERNAME, DB_PWD );
 			$this->dbh->setAttribute ( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 		} catch ( PDOException $e ) {
 			error_log($e);
-			echo $e;
+			$GLOBALS['html_template'] = $this->utils ->showErrorMsg($GLOBALS['html_template'], $e->getMessage() . PHP_EOL);
 		}
 	}
 	
-	// Retrieve all records stored in the database table DB_PHOTO_TABLE_NAME. Return an array of Photo objects
 	public function getAllPhotos() {
 		$photos = array ();
 		try {
 			$stm = $this->dbh->query ( 'SELECT * FROM ' . DB_PHOTO_TABLE_NAME );
-			
-			if(!$stm){
-				$this->dbh->query('CREATE TABLE photo_metadata ( title VARCHAR(30), description VARCHAR(30), creationdate VARCHAR(30), keywords VARCHAR(30), reference VARCHAR(30) );');
-			
-			}
-			
 			foreach ( $stm as $row ) {
 				array_push ( $photos, new Photo ( $row [DB_PHOTO_TITLE_COL_NAME], 
 												$row [DB_PHOTO_DESCRIPTION_COL_NAME],
@@ -44,7 +39,41 @@ class MyDB
 			return $photos;
 		} catch ( PDOException $e ) {
 			error_log($e);
-			echo $e;
+			$GLOBALS['html_template'] = $this->utils ->showErrorMsg($GLOBALS['html_template'], $e->getMessage() . PHP_EOL);
+		}
+	}
+	
+	public function addPhoto($photo) {
+		try {
+			$sql = "INSERT INTO ".DB_PHOTO_TABLE_NAME." (".DB_PHOTO_TITLE_COL_NAME.", ".DB_PHOTO_DESCRIPTION_COL_NAME.", ".DB_PHOTO_CREATIONDATE_COL_NAME.", ".DB_PHOTO_KEYWORDS_COL_NAME.", ".DB_PHOTO_S3REFERENCE_COL_NAME .") VALUES(?,?,?,?,?);";
+			$this->dbh->prepare ( $sql )->execute ( [
+					$photo->getName(),
+					$photo->getDescription(),
+					$photo->getCreationDate(),
+					$photo->getKeywords(),
+					$photo->getS3Reference(),
+			] );
+			return $this->dbh->lastInsertId();
+		} catch ( PDOException $e ) {
+			error_log($e);
+			$GLOBALS['html_template'] = $this->utils ->showErrorMsg($GLOBALS['html_template'], $e->getMessage() . PHP_EOL);
+		}
+	}
+	
+	public function getPhotoByName($name){
+		try {
+			$stmt = $this->dbh->prepare("SELECT * FROM ".DB_PHOTO_TABLE_NAME." WHERE ".DB_PHOTO_TITLE_COL_NAME."='$name' LIMIT 1");
+			$stmt->execute();
+			$row = $stmt->fetch();
+			if($row){
+				$photo = new Photo ( $row [DB_PHOTO_TITLE_COL_NAME], $row [DB_PHOTO_DESCRIPTION_COL_NAME]) ;
+				return $photo;
+			}else{
+				return null;
+			}
+		} catch ( PDOException $e ) {
+			error_log($e);
+			$GLOBALS['html_template'] = $this->utils ->showErrorMsg($GLOBALS['html_template'], $e->getMessage() . PHP_EOL);
 		}
 	}
 }
